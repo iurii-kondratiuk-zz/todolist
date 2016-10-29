67951ccf91bf887f6842491a93376e6d95f8a67e
iurii-kondratiuk <uvkondratiuk@gmail.com>
2016-10-29 12:38:05 +0200

    implement unit test for Checkbox

---
+++ src/components/Checkbox/Checkbox.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Checkbox from './Checkbox';
+
+describe('<Checkbox />', () => {
+	it('should be rendered unchecked', () => {
+	  const wrapper = shallow(<Checkbox checked={false} />);
+	  expect(wrapper.hasClass('Checkbox--checked')).toEqual(false);
+	});
+
+	it('should be rendered checked', () => {
+	  const wrapper = shallow(<Checkbox checked={true} />);
+	  expect(wrapper.hasClass('Checkbox--checked')).toEqual(true);
+	});
+
+	it('should call a callback upon click', () => {
+		const callback = jest.fn();
+	  const wrapper = shallow(<Checkbox checked={false} onChange={callback} />);
+	  wrapper.find('.Checkbox').simulate('click');
+	  expect(callback.mock.calls.length).toEqual(1);
+	});
+});
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import Header from './Header';
 
 describe('<Header />', () => {
-	it('renders without crashing', () => {
+	it('should render correct title', () => {
 		const title = 'Inbox';
 	  const wrapper = shallow(<Header title={title} />);
 	  expect(wrapper.text()).toEqual(title);
